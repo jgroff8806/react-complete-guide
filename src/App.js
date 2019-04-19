597946aa21d2785883b1ae3faccaa7a595bf4cd2
C_Groff, Jason <C_Groff.Jason@Cerner.com>
2019-04-19 15:39:36 -0500

    Creating a Functional Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>React Complete Guide</h1>
+        <p>This is a nested paragraph</p>
+        <Person />
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Is this working?'));
