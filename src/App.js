ea3c5c2121989ec6d39a3eb497cedcdcee05207c
C_Groff, Jason <C_Groff.Jason@Cerner.com>
2019-04-19 16:02:05 -0500

    Understanding & Using State

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [{ name: 'Jason', age: 31 }, { name: 'Jayson', age: 32 }, { name: 'Json', age: 45 }]
+  };
+
   render() {
     return (
       <div className="App">
         <h1>React Complete Guide</h1>
         <p>This is a nested paragraph</p>
-        <Person name="Jason" age="31" />
-        <Person name="Jayson" age="32" />
-        <Person name="Json" age="45" />
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
+          My Hobbies: Being confused with Jason
+        </Person>
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Is this working?'));
