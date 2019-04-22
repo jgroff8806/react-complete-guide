7e0de39d6d65082c465ade1bb6593ab2ba826d3f
C_Groff, Jason <C_Groff.Jason@Cerner.com>
2019-04-22 13:48:38 -0500

    Working with inline styles

--- src/App.js
+++ src/App.js
@@ -21,11 +21,21 @@ class App extends Component {
   };
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>React Complete Guide</h1>
         <p>This is a nested paragraph</p>
-        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+        <button style={style} onClick={() => this.switchNameHandler('Maximilian!!')}>
+          Switch Name
+        </button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person
           name={this.state.persons[1].name}
